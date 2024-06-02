7baa14929550980e79455a8119102075aa75def3
Mn <youfirst@hotmail.com>
2024-06-01 21:29:26 -0500

    lw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaginaComponent } from './pages/pagina/pagina.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PaginaComponent } from './pages/pagina/pagina.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'nacionales/operativo-conjunto-desmantela-red-de-narcotrafico-desde-panama-hacia-europa-y-asia_1_2130162.html', component: PaginaComponent},
+    { path: '', redirectTo: 'nacionales/operativo-conjunto-desmantela-red-de-narcotrafico-desde-panama-hacia-europa-y-asia_1_2130162.html', pathMatch: 'full'}
+];
